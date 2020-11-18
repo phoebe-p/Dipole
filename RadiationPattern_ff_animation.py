65b899a7890d40b5290ab0196de08275d14c22b3
phoebe-p <phoebe.pearce15@imperial.ac.uk>
2020-11-18 18:07:17 +0000

    update plots.ipynb

--- RadiationPattern_ff_animation.py
+++ RadiationPattern_ff_animation.py
@@ -109,71 +109,74 @@ plt.show()
 
 
 
-
-
-
-nt = 11
-nph = 10
-
-theta = np.arccos(2 * np.linspace(0, 1, nt) - 1)
-theta = theta[theta <= np.pi/2]
-phi = np.linspace(2 * np.pi / nph, 2*np.pi, nph)
-
-r0 = 1
-th_phi = np.meshgrid(theta, phi)
-thetas = th_phi[0].flatten()
-phis = th_phi[1].flatten()
-
-x = r0 * np.sin(thetas) * np.cos(phis)
-y = r0 * np.sin(thetas) * np.sin(phis)
-z = r0 * np.cos(thetas)
-
-T = 1 / 1e8
-
-p = np.array([[1e-8, 0, 0], [0, 1e-8, 0]])
-R = np.array([[0, 0, 0], [0, 0, 0]])
-f = np.array([1e8, 1e8])
-t = 0
-phi = np.array([0, np.pi/2])
-
-E = np.zeros((3, len(x)))
-B = np.zeros((3, len(x)))
-
-for j1 in np.arange(0, len(x)):
-  El, Bl = Magnetic_dipole_ff(np.array([x[j1], y[j1], z[j1]]), p, R, phi, f, t=t)
-  E[:, j1] = El[:, 0]
-  B[:, j1] = Bl[:, 0]
-
-Emag = np.sqrt(sum(E ** 2, 0))
-E_max_mag = max(Emag)
-
-Bmag = np.sqrt(sum(B ** 2, 0))
-B_max_mag = max(Bmag)
-
-scale_E = 20
-scale_B = 7*E_max_mag/B_max_mag
-
-fig1 = plt.figure(2)
-ax1 = fig1.gca(projection='3d')
-Q1 = ax1.quiver(x, y, z, *E, pivot='middle')
-Q2 = ax1.quiver(x, y, z, *B, pivot='middle', color='black')
-
-ax1.set_xlim(-1.1*r0, 1.1*r0)
-ax1.set_ylim(-1.1*r0, 1.1*r0)
-ax1.set_zlim(-1.1*r0, 1.1*r0)
-
-ax1.set_xlabel('x')
-ax1.set_ylabel('y')
-ax1.set_zlabel('z')
-
-
-scale = r0/2000
-scale_B = (E_max_mag/B_max_mag)*scale
-# you need to set blit=False, or the first set of arrows never gets
-# cleared on subsequent frames
-anim = FuncAnimation(fig1, update_quiver, fargs=(Q1, Q2, x, y, z, scale, scale_B),
-                               interval=100, blit=False)
-ax1.view_init(elev=20., azim=20)
-fig1.tight_layout()
-plt.show()
-
+# 
+#
+#
+# nt = 11
+# nph = 10
+#
+# theta = np.arccos(2 * np.linspace(0, 1, nt) - 1)
+# theta = theta[theta <= np.pi/2]
+# phi = np.linspace(2 * np.pi / nph, 2*np.pi, nph)
+#
+# r0 = 1
+# th_phi = np.meshgrid(theta, phi)
+# thetas = th_phi[0].flatten()
+# phis = th_phi[1].flatten()
+#
+# x = r0 * np.sin(thetas) * np.cos(phis)
+# y = r0 * np.sin(thetas) * np.sin(phis)
+# z = r0 * np.cos(thetas)
+#
+# T = 1 / 1e8
+#
+# p = np.array([[1e-8, 0, 0], [0, 1e-8, 0]])
+# R = np.array([[0, 0, 0], [0, 0, 0]])
+# f = np.array([1e8, 1e8])
+# t = 0
+# phi = np.array([0, np.pi/2])
+#
+# E = np.zeros((3, len(x)))
+# B = np.zeros((3, len(x)))
+#
+# for j1 in np.arange(0, len(x)):
+#   El, Bl = Magnetic_dipole_ff(np.array([x[j1], y[j1], z[j1]]), p, R, phi, f, t=t)
+#   E[:, j1] = El[:, 0]
+#   B[:, j1] = Bl[:, 0]
+#
+# Emag = np.sqrt(sum(E ** 2, 0))
+# E_max_mag = max(Emag)
+#
+# Bmag = np.sqrt(sum(B ** 2, 0))
+# B_max_mag = max(Bmag)
+#
+# scale_E = 20
+# scale_B = 7*E_max_mag/B_max_mag
+#
+# fig1 = plt.figure(2)
+# ax1 = fig1.gca(projection='3d')
+# Q1 = ax1.quiver(x, y, z, *E, pivot='middle')
+# Q2 = ax1.quiver(x, y, z, *B, pivot='middle', color='black')
+#
+# ax1.set_xlim(-1.1*r0, 1.1*r0)
+# ax1.set_ylim(-1.1*r0, 1.1*r0)
+# ax1.set_zlim(-1.1*r0, 1.1*r0)
+#
+# ax1.set_xlabel('x')
+# ax1.set_ylabel('y')
+# ax1.set_zlabel('z')
+#
+#
+# scale = r0/2000
+# scale_B = (E_max_mag/B_max_mag)*scale
+# # you need to set blit=False, or the first set of arrows never gets
+# # cleared on subsequent frames
+# anim = FuncAnimation(fig1, update_quiver, fargs=(Q1, Q2, x, y, z, scale, scale_B),
+#                                interval=100, blit=False)
+# ax1.view_init(elev=20., azim=20)
+# fig1.tight_layout()
+# plt.show()
+#
+#
+#
+# anim.save('rotating_magnetic_dipole_.gif', writer='imagemagick', fps=30)
